20d7667168b9f706201877e71ee0a84a19beb8a7
Nate Genter <ngenter@plasticingenuity.local>
2016-12-06 18:15:00 -0600

    Set up Navbar/header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipeBookComponent } from './recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, RecipeBookComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipe-book.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HeaderComponent } from './header.component';
+
+@Component({
+  selector: 'recipe-book-app',
+  templateUrl: 'recipe-book.component.html'
+})
+export class RecipeBookComponent {
+
+}
