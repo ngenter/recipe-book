1dc0111a0da531fd29d86beb2796ef16a4e23d31
Nate Genter <ngenter@plasticingenuity.local>
2016-12-06 18:26:16 -0600

    Added Recipes Component and 2 column layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { RecipeBookComponent } from './recipe-book.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, RecipeBookComponent,
+    AppComponent, HeaderComponent, RecipeBookComponent, RecipesComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/recipe-book.component.ts
+++ src/app/recipe-book.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes'
 
 @Component({
   selector: 'recipe-book-app',
