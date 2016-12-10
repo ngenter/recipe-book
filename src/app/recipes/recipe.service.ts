840261d15608ed13f3afd96d4e377afcb5992ef7
Nate Genter <ngenter@plasticingenuity.local>
2016-12-10 12:48:46 -0600

    Added Cross Service Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Recipe } from '../recipes/recipe'
+import { Recipe } from '../recipes/recipe';
+import { Ingredient } from '../shared/ingredient'
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', [] ),
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1),
+    ] ),
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
 
